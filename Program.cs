aafc3b4d935915dbce0382fa319f52b1f375043a
Maks <maksbolshoy@mail.ru>
2022-11-30 21:58:49 +0300

    obch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат .
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // тип данных имя = значение;
+
+// переменная в которой хранится результат;
+int result = number * number; // result =number^2
+Console.WriteLine(result);
+
+
+
+
+
